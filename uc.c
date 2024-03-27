4de50bf2cff3292a82991b1e9b9f7ed9dbcb2c51
Glenn Baker <glenn.baker@gmx.com>
2024-03-27 21:11:55 +0100

    Add Unicorn/AVR support.
    Signed-off-by: Glenn Baker <glenn.baker@gmx.com>

--- uc.c
+++ uc.c
@@ -26,6 +26,7 @@
 #include "qemu/target/riscv/unicorn.h"
 #include "qemu/target/s390x/unicorn.h"
 #include "qemu/target/tricore/unicorn.h"
+#include "qemu/target/avr/unicorn.h"
 
 #include "qemu/include/qemu/queue.h"
 #include "qemu-common.h"
@@ -181,6 +182,10 @@ bool uc_arch_supported(uc_arch arch)
 #endif
 #ifdef UNICORN_HAS_TRICORE
     case UC_ARCH_TRICORE:
+        return true;
+#endif
+#ifdef UNICORN_HAS_AVR
+    case UC_ARCH_AVR:
         return true;
 #endif
     /* Invalid or disabled arch */
@@ -407,6 +412,15 @@ uc_err uc_open(uc_arch arch, uc_mode mode, uc_engine **result)
                 return UC_ERR_MODE;
             }
             uc->init_arch = tricore_uc_init;
+            break;
+#endif
+#ifdef UNICORN_HAS_AVR
+        case UC_ARCH_AVR:
+            if ((mode & ~UC_MODE_AVR_MASK)) {
+                free(uc);
+                return UC_ERR_MODE;
+            }
+            uc->init_arch = avr_uc_init;
             break;
 #endif
         }
@@ -835,6 +849,11 @@ uc_err uc_emu_start(uc_engine *uc, uint64_t begin, uint64_t until,
 #ifdef UNICORN_HAS_TRICORE
     case UC_ARCH_TRICORE:
         uc_reg_write(uc, UC_TRICORE_REG_PC, &begin_pc32);
+        break;
+#endif
+#ifdef UNICORN_HAS_AVR
+    case UC_ARCH_AVR:
+        uc_reg_write(uc, UC_AVR_REG_PC, &begin_pc32);
         break;
 #endif
     }
@@ -2036,6 +2055,12 @@ static void find_context_reg_rw_function(uc_arch arch, uc_mode mode,
     case UC_ARCH_TRICORE:
         rw->context_reg_read = tricore_context_reg_read;
         rw->context_reg_write = tricore_context_reg_write;
+        break;
+#endif
+#ifdef UNICORN_HAS_AVR
+    case UC_ARCH_AVR:
+        rw->context_reg_read = avr_context_reg_read;
+        rw->context_reg_write = avr_context_reg_write;
         break;
 #endif
     }
